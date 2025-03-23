66a7dac0abfb1cfa81db7134300420d73fb132f6
Geetha-005 <chillageetha323@gmail.com>
2025-03-23 20:24:17 +0530

    day 2 added

---
+++ JAVA/day 2/Loops.java
@@ -0,0 +1,9 @@
+class Loops {
+    public static void main(String[] args) {
+        System.out.println("Hello Krishna");
+
+        for(int i=0;i<5;i++){
+            System.out.println("Hello Krishna");
+        }
+    }
+}

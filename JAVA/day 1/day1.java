115051ef6d2a42d2128fbe881310ab3ae43c6885
Geetha-005 <chillageetha323@gmail.com>
2025-03-24 16:11:01 +0530

    day 3 added

--- JAVA/day 1/day1.java
+++ JAVA/day 1/day1.java
@@ -1,6 +1,25 @@
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Krishna");
+// language fundamnetals
+
+// variables and datatypes
+        System.out.println("radha Krishna");
+
+        int x=90;
+        float y=90.89f;
+        boolean z=true;
+        System.out.println(x+""+y+""+z);
+        char a='a';
+        char name='s';
+        System.out.println(name);
+        System.out.println(a);
+
+        // explicit type conversion
+        int age=27;
+        short newAge=(short)age;
+        System.out.println(newAge);
+        
+
     }
 }
---
+++ JAVA/day3/Arrays.java
@@ -0,0 +1,9 @@
+package JAVA.day3;
+
+public class Arrays {
+    public static void main(String [] args){
+        System.out.println("hare krishna");
+        
+    }
+    
+}

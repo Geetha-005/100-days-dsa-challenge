ed759fee549e20c50727183d7f842f243d1b8135
Geetha-005 <chillageetha323@gmail.com>
2025-05-13 23:28:59 +0530

    day 2 problem of the day is done

--- PYTHON/daily leetcode pod/Streak.py
+++ PYTHON/daily leetcode pod/Streak.py
@@ -25,3 +25,61 @@ def findEvenNumbers(digits):
         return res
         
 print(findEvenNumbers([2,1,3,0]))
+
+
+
+
+# 13-05-2025 
+
+#3335 - total characters in string after transformations I
+
+# Input: s = "abcyy", t = 2
+
+# Output: 7
+
+# Explanation:
+
+# First Transformation (t = 1):
+# 'a' becomes 'b'
+# 'b' becomes 'c'
+# 'c' becomes 'd'
+# 'y' becomes 'z'
+# 'y' becomes 'z'
+# String after the first transformation: "bcdzz"
+# Second Transformation (t = 2):
+# 'b' becomes 'c'
+# 'c' becomes 'd'
+# 'd' becomes 'e'
+# 'z' becomes "ab"
+# 'z' becomes "ab"
+# String after the second transformation: "cdeabab"
+# Final Length of the string: The string is "cdeabab", which has 7 characters.
+
+
+ # code --------------------------------------
+
+ class Solution:
+    def lengthAfterTransformations(self, s: str, t: int) -> int:
+        mod = 10**9 + 7
+        nums = [0]*26
+        for ch in s:
+            nums[ord(ch) - 97] += 1
+
+        for _ in range(t):
+            cur = [0]*26
+            z = nums[25]
+            if z:
+                # 'z' → 'a' and 'b'
+                cur[0] = (cur[0] + z) % mod
+                cur[1] = (cur[1] + z) % mod
+            for j in range(25):
+                v = nums[j]
+                if v:
+                    cur[j+1] = (cur[j+1] + v) % mod
+            nums = cur
+
+        res = 0
+        for v in nums:
+            res = (res + v) % mod
+        return res
+        
